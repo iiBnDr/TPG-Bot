be4f974546c6d50c4677450f61418f4e9ee5d8da
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2018-12-30 22:05:33 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var adminprefix = '#'
+var prefix = "#";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : :ok_hand: عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$bc | TyPrixTM Shop`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`🌹  ولكم نورت السيرفر🌹 
+👑اسم العضو  ${member}👑  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
+client.on('message' , message => {//Toxic Codes
+  if(message.author.bot) return;//Toxic Codes
+  if(message.content.startsWith(prefix + "ping")) {//Toxic Codes
+ message.channel.send('pong').then((msg) => {//Toxic Codes
+var PinG = `${Date.now() - msg.createdTimestamp}`
+var ApL = `${Math.round(client.ping)}`//Toxic Codes
+      msg.edit(`\`\`\`javascript\nTime taken: ${PinG} ms.\nDiscord API: ${ApL} ms.\`\`\``);//Toxic Codes
+ })//Toxic Codes
+  }  //Toxic Codes
+ });//Toxic Codes
+
+
+client.login(process.env.BOT_TOKEN);
