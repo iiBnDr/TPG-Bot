e040aa95f1aabbbf36f6759e3253d515178c7591
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2018-12-30 22:09:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc | TyPrixTM Shop`,"http://twitch.tv/S-F")
+client.user.setGame(`#bc | TheProGamers Bot`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
